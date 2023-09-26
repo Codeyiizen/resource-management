3b99c5b652293a11eeb304d2f5e95f4975e4c883
subhash018 <59439792+subhash018@users.noreply.github.com>
2023-09-26 17:23:31 +0530

    Time Tracking page
    Time Tracking page changes

--- src/Screens/TimeTracker.tsx
+++ src/Screens/TimeTracker.tsx
@@ -65,7 +65,7 @@ export default class Workorder extends Component<{}, WorkorderStateInterface>{
                         <View>
                             <View style={{ marginBottom: 50 }}>
                                 <Text style={[ThemeStyling.heading5, { textAlign: "center", marginBottom: 0, color: Colors.dark_color, fontWeight: '600', fontFamily: 'Poppins_600SemiBold', }]}>Punch Clock</Text>
-                                <Text style={[ThemeStyling.text2, { color: Colors.success_color, textAlign: "center" }]}>September 25, 2023</Text>
+                                <Text style={[ThemeStyling.text2, { color: Colors.success_color, textAlign: "center" }]}>25 Sep at 4:24 PM</Text>
                             </View>
                             <View style={[ThemeStyling.threeColumnLayout, { marginBottom: 50 }]}>
                                 <View>
@@ -73,15 +73,15 @@ export default class Workorder extends Component<{}, WorkorderStateInterface>{
                                     <Text style={[ThemeStyling.text2, { color: Colors.secondry_color, lineHeight: 15, textAlign: "center", textTransform: "uppercase" }]}>Start Time</Text>
                                 </View>
                                 <View>
-                                    <FontAwesome name="circle" size={20} style={{ color: Colors.success_color }} />
-                                    <Text style={[ThemeStyling.text2, { textAlign: "center", color: Colors.success_color }]}>IN</Text>
+                                    <FontAwesome name="circle" size={20} style={{ color: Colors.errorColor }} />
+                                    <Text style={[ThemeStyling.text2, { textAlign: "center", color: Colors.errorColor, textTransform:"uppercase" }]}>Out</Text>
                                 </View>
                                 <View>
                                     <Text style={[ThemeStyling.heading4, { marginBottom: 0, lineHeight: 22 }]}>0:10:00</Text>
                                     <Text style={[ThemeStyling.text2, { color: Colors.secondry_color, lineHeight: 15, textAlign: "center", textTransform: "uppercase" }]}>Duration</Text>
                                 </View>
                             </View>
-                            <View style={{marginBottom:20}}>
+                            <View style={{ marginBottom: 20 }}>
                                 <View>
                                     <Text style={[ThemeStyling.heading2, { textAlign: "center", marginBottom: 5 }]}>Jack Cooper</Text>
                                 </View>
@@ -96,14 +96,80 @@ export default class Workorder extends Component<{}, WorkorderStateInterface>{
                             </View>
 
                             <View style={{ justifyContent: 'center', alignItems: "center", flex: 1, marginBottom: 20 }}>
-                                <TouchableOpacity style={[ThemeStyling.btnSuccess, { width: 150 }]}>
-                                    <Text style={[ThemeStyling.btnText, { fontSize: Colors.FontSize.f16, color: Colors.white }]}>Punch In</Text>
+                                <TouchableOpacity style={[ThemeStyling.btnSuccess, { width: 150, height: 45, alignItems: "center", flexDirection: "row" }]}>
+                                    <FontAwesome name="hand-o-right" size={24} color="white" />
+                                    <Text style={[ThemeStyling.btnText, { fontSize: Colors.FontSize.f16, color: Colors.white, marginLeft: 10 }]}>Punch In</Text>
                                 </TouchableOpacity>
                             </View>
 
-                            <View style={{ justifyContent: "center", flex: 1, flexDirection: "row" }}>
+                            <View style={{ justifyContent: "center", flex: 1, flexDirection: "row", marginBottom:10 }}>
                                 <FontAwesome name="commenting-o" size={20} style={{ color: Colors.primary_color, marginRight: 5 }} />
-                                <Text>Note</Text>
+                                <Text>Notes</Text>
+                            </View>
+                            <View>
+                                <View style={[ThemeStyling.twoColumnLayout, { justifyContent: "space-between", marginBottom: 5 }]}>
+                                    <View>
+                                        <Text style={[ThemeStyling.heading5, {
+                                            color: Colors.dark_color, fontFamily: 'Poppins_700Bold',
+                                            fontWeight: '700',
+                                        }]}>Today's Punch</Text>
+                                    </View>
+                                    <View>
+                                        <Text style={[ThemeStyling.heading5, {
+                                            color: Colors.dark_color, fontFamily: 'Poppins_700Bold',
+                                            fontWeight: '700',
+                                        }]}>Timeline</Text>
+                                    </View>
+                                </View>
+                                <View style={[ThemeStyling.twoColumnLayout, { justifyContent: "space-between" }]}>
+                                    <View style={ThemeStyling.twoColumnLayout}>
+                                        <View style={{ marginRight: 5 }}>
+                                            <MaterialCommunityIcons name="timer-outline" size={18} style={{ color: Colors.primary_color }} /></View>
+                                        <View>
+                                            <Text style={[ThemeStyling.text2, { color: Colors.dark_color }]}>Check In</Text>
+                                        </View>
+                                    </View>
+                                    <View>
+                                        <Text style={[ThemeStyling.text2, { color: Colors.secondry_color }]}>9:45 AM</Text>
+                                    </View>
+                                </View>
+                                <View style={[ThemeStyling.twoColumnLayout, { justifyContent: "space-between" }]}>
+                                    <View style={ThemeStyling.twoColumnLayout}>
+                                        <View style={{ marginRight: 5 }}>
+                                            <MaterialIcons name="timer-off" size={16} style={{ color: Colors.errorColor }} /></View>
+                                        <View>
+                                            <Text style={[ThemeStyling.text2, { color: Colors.dark_color }]}>Check Out</Text>
+                                        </View>
+                                    </View>
+                                    <View>
+                                        <Text style={[ThemeStyling.text2, { color: Colors.secondry_color }]}>12:30 PM</Text>
+                                    </View>
+                                </View>
+
+                                <View style={[ThemeStyling.twoColumnLayout, { justifyContent: "space-between" }]}>
+                                    <View style={ThemeStyling.twoColumnLayout}>
+                                        <View style={{ marginRight: 5 }}>
+                                            <MaterialCommunityIcons name="timer-outline" size={18} style={{ color: Colors.primary_color }} /></View>
+                                        <View>
+                                            <Text style={[ThemeStyling.text2, { color: Colors.dark_color }]}>Check In</Text>
+                                        </View>
+                                    </View>
+                                    <View>
+                                        <Text style={[ThemeStyling.text2, { color: Colors.secondry_color }]}>8:15 AM</Text>
+                                    </View>
+                                </View>
+                                <View style={[ThemeStyling.twoColumnLayout, { justifyContent: "space-between" }]}>
+                                    <View style={ThemeStyling.twoColumnLayout}>
+                                        <View style={{ marginRight: 5 }}>
+                                            <MaterialIcons name="timer-off" size={16} style={{ color: Colors.errorColor }} /></View>
+                                        <View>
+                                            <Text style={[ThemeStyling.text2, { color: Colors.dark_color }]}>Check Out</Text>
+                                        </View>
+                                    </View>
+                                    <View>
+                                        <Text style={[ThemeStyling.text2, { color: Colors.secondry_color }]}>10:30 AM</Text>
+                                    </View>
+                                </View>
                             </View>
                         </View>
                     </View>
